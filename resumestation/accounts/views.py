ae2d09c5978632d98045d10d0e7ca24417abd3fc
iihsan <iihsanofficial@gmail.com>
2021-10-05 22:51:14 +0500

    Fixing login/signup/logout pages

--- resumestation/accounts/views.py
+++ resumestation/accounts/views.py
@@ -4,22 +4,32 @@ from django.contrib import auth
 
 # Create your views here.
 def signup(request):
+  if request.user.is_authenticated:
+    return redirect('home')
+    
   if request.method == 'POST':
-    if request.POST['password'] == request.POST['confirmpassword']:
-      try:
-        user = User.objects.get(username = request.POST['username'])
-        return render(request, 'accounts/signup.html', {'error' : 'Username has been already taken'})
-      except User.DoesNotExist:
-        user = User.objects.create_user(username =request.POST['username'], password=request.POST['password'], email=request.POST['email'])
-        auth.login(request, user)
-        return redirect('home')
+    if request.POST['username'] and request.POST['email'] and request.POST['password'] and request.POST['confirmpassword']:
+      if request.POST['password'] == request.POST['confirmpassword']:
+        try:
+          user = User.objects.get(username = request.POST['username'])
+          return render(request, 'accounts/signup.html', {'error' : 'Username has been already taken'})
+        except User.DoesNotExist:
+          user = User.objects.create_user(username =request.POST['username'], password=request.POST['password'], email=request.POST['email'])
+          auth.login(request, user)
+          return redirect('home')
+      else:
+        return render(request, 'accounts/signup.html', {'error' : 'Passwords are not correct'})
+
     else:
-      return render(request, 'accounts/signup.html', {'error' : 'Passwords must match'})
+      return render(request, 'accounts/signup.html', {'error' : 'Please fill all fields'})
 
   else:
     return render (request, 'accounts/signup.html')
 
 def login(request):
+  if request.user.is_authenticated:
+    return redirect('home')
+
   if request.method == 'POST':
     user = auth.authenticate(username=request.POST['username'], password=request.POST['password'], email=request.POST['email'])
     if user is not None:
@@ -27,11 +37,12 @@ def login(request):
       return redirect('home')
     else:
       return render(request, 'accounts/login.html', {'error': 'username or password is incorrect.'})
+  
   else:
     return render (request, 'accounts/login.html')
 
 def logout(request):
-  if request.method == 'POST':
+  if request.method == 'POST' and request.user.is_authenticated:
     auth.logout(request)
-    return redirect('home')
+  return redirect('home')
   
